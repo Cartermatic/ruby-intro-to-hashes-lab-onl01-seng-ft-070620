92ed9a29582a57476b185627ce9e72656babfa4f
Carter Parrett <cparrett94@hotmail.com>
2020-07-12 04:54:14 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -17,7 +17,10 @@ def monopoly_with_second_tier
 end
 
 def monopoly_with_third_tier
-	monopoly = {railroads: {pieces: 4, names: {reading_railroad: {},
+	monopoly = 
+	{railroads:
+	{pieces: 4,
+	names: {reading_railroad: {},
 	pennsylvania_railroad: {},
 	b_and_o_railroad: {},
 	shortline: {}},
